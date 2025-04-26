c66474411d21ec6574685d1d40fb6e45011cf551
matheus-santos-da-silva <maatheus.santos223@gmail.com>
2025-04-26 20:46:20 -0300

    fix: return price as a number

--- backend/src/application/use-cases/products/get-products-use-case.ts
+++ backend/src/application/use-cases/products/get-products-use-case.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { Decimal } from "@prisma/client/runtime/library";
 import { Product } from "src/domain/entities/product";
 import { GetProductsProtocol } from "src/domain/use-cases-protocol/products/get-products";
 import { ProductsRepositoryProtocol } from "src/infra/database/repositories/protocols/products-repository-protocol";
@@ -10,6 +11,15 @@ export class GetProductsUseCase implements GetProductsProtocol {
   async findMany(): Promise<Product[] | []> {
     try {
       const products = await this.repository.findMany();
+      if (products.length > 0) {
+        const newProducts = products.map((product) => ({
+          ...product,
+          price: new Decimal(product.price).toNumber(),
+        }));
+
+        return newProducts;
+      }
+
       return products;
     } catch (error) {
       throw error;
