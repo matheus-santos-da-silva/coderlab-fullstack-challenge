5c8a9ef6e94bcf6b8ccb4cae7973d2794a4efe40
matheus-santos-da-silva <maatheus.santos223@gmail.com>
2025-04-26 11:29:25 -0300

    feat: add categories hook

---
+++ frontend/src/hooks/useCategories.tsx
@@ -0,0 +1,31 @@
+import { Category } from "@/shared/DTOS/category-dto";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const useCategories = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_SOME_KEY.BACKEND_URL}/category`
+        );
+        setCategories(response.data.categories);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(
+            error.response?.data?.message || "Erro ao trazer as Categorias."
+          );
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    getCategories();
+  }, []);
+
+  return { categories, loading, error };
+};
