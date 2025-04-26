67d40f2653f8e9b2e5a8a4cd75d8b0e5acdf2338
matheus-santos-da-silva <maatheus.santos223@gmail.com>
2025-04-26 20:46:46 -0300

    feat: add url to upload product images

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { NestExpressApplication } from "@nestjs/platform-express";
+import { join } from "path";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  const uploadsPath = join(process.cwd(), "uploads");
+
+  app.useStaticAssets(uploadsPath, {
+    prefix: "/uploads/",
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
