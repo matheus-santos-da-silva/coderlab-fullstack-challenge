f8d0692463e3f2b402f11f1ea390e9a8a0b0f5fa
matheus-santos-da-silva <maatheus.santos223@gmail.com>
2025-04-28 08:47:58 -0300

    fix: add product-id exists validation

--- backend/src/application/use-cases/products/update-product-use-case.ts
+++ backend/src/application/use-cases/products/update-product-use-case.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { UpdateProductDTO } from "src/domain/DTO/update-product-DTO";
 import { UpdateProductProtocol } from "src/domain/use-cases-protocol/products";
 import { ProductsRepositoryProtocol } from "src/infra/database/repositories/protocols/products-repository-protocol";
@@ -9,6 +9,8 @@ export class UpdateProductUseCase implements UpdateProductProtocol {
 
   async update(id: string, data: UpdateProductDTO): Promise<void> {
     try {
+      const product = await this.repository.find(id);
+      if (!product) throw new BadRequestException("Produto não existe!");
       await this.repository.update(id, data);
     } catch (error) {
       throw error;
